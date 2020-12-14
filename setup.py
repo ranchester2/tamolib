093cf8917540e70e6df5fe2ad9bf8775f8b7130b
Sabreh Vardas <sabrehvardas@gmail.com>
2020-12-14 12:37:39 +0200

    bump version to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tamo",
-    version='0.0.1',
+    version='0.0.2',
     author="Sabreh Vardas",
     author_email="savrehvardas@gmail.com",
     description="A package for interacting with TAMO",
